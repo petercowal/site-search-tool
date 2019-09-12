0d118c574ca6b8ef3d6e685124de6809a746dd94
Peter Cowal <pcowal@gmail.com>
2019-09-11 20:57:43 -0700

    adds another unicode quote replacement

--- cleanup.py
+++ cleanup.py
@@ -4,7 +4,8 @@ def cleanUpString(str):
     unicodeEquivalents = {
         '“' : '\"',
         '”' : '\"',
-        '’' : '\''
+        '’' : '\'',
+        '‘' : '\''
     }
     for key in unicodeEquivalents:
         str = str.replace(key,unicodeEquivalents[key])
